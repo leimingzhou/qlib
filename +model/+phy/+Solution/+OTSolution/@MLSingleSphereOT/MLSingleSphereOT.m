5cd6305f00eea6f4c7ea109c5eaa65bbeae27312
Lei-Ming Zhou <lm.zhou2@gmail.com>
2016-03-22 20:20:19 +0800

    for sync
    rt

--- +model/+phy/+Solution/+OTSolution/@MLSingleSphereOT/MLSingleSphereOT.m
+++ +model/+phy/+Solution/+OTSolution/@MLSingleSphereOT/MLSingleSphereOT.m
@@ -1,4 +1,4 @@
-classdef MLSingleSphereOT  < model.phy.Solution.OTsolution.AbstractOTSolution
+classdef MLSingleSphereOT  < model.phy.Solution.OTSolution.AbstractOTSolution
     %SINGLESPHEREOPTICALTWEEZERS Summary of this class goes here
     %   Detailed explanation goes here
     
@@ -8,28 +8,15 @@ classdef MLSingleSphereOT  < model.phy.Solution.OTsolution.AbstractOTSolution
     
     methods
         function obj=MLSingleSphereOT(xml_file)
-            obj@model.phy.Solution.OTsolution.AbstractOTSolution(xml_file);
+            obj@model.phy.Solution.OTSolution.AbstractOTSolution(xml_file);
         end
         
         function get_parameters(obj, p)
-            get_parameters@model.phy.Solution.OTsolution.AbstractOTSolution(obj, p);
-            
-%             obj.parameters.FocalDistance = p.get_parameter('Lens', 'FocalDistance');
-%             obj.parameters.WorkingMedium = p.get_parameter('Lens', 'Medium');
-%             obj.parameters.NA            = p.get_parameter('Lens', 'NA');
-%             
-%             obj.parameters.IncBeamCenter = p.get_parameter('IncBeam', 'Center');
-%             obj.parameters.IncBeamP_L    = p.get_parameter('IncBeam', 'P_L');
-%             obj.parameters.IncBeamPower  = p.get_parameter('IncBeam', 'Power');
-%             obj.parameters.IncBeamPxPy   = p.get_parameter('IncBeam', 'PxPy');
-%             obj.parameters.IncBeamWaist  = p.get_parameter('IncBeam', 'Waist');
-%             obj.parameters.IncBeamWaveLength  = p.get_parameter('IncBeam', 'WaveLength');
+            get_parameters@model.phy.Solution.OTSolution.AbstractOTSolution(obj, p);      
             %For scatterer
             obj.parameters.SpherePosition = p.get_parameter('MultiLayerSphereScatterer', 'Position');
             obj.parameters.SphereRadius   = p.get_parameter('MultiLayerSphereScatterer', 'Radius');
             obj.parameters.SphereMedium   = p.get_parameter('MultiLayerSphereScatterer', 'Medium');
-            
-%             obj.parameters.CutOffNMax = p.get_parameter('CutOff', 'NMax');
         end
         
         function [force,torque,forceQ, torqueQ]=perform(obj)
