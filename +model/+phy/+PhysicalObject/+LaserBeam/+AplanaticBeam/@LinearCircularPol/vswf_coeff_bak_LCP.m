3273488d0691965c221d3fc5a1708970914084e3
Lei-Ming Zhou <lm.zhou2@gmail.com>
2016-06-03 22:43:53 +0800

    for sync
    rt

--- +model/+phy/+PhysicalObject/+LaserBeam/+AplanaticBeam/@LinearCircularPol/vswf_coeff_bak_LCP.m
+++ +model/+phy/+PhysicalObject/+LaserBeam/+AplanaticBeam/@LinearCircularPol/vswf_coeff_bak_LCP.m
@@ -2,19 +2,13 @@ function [ pmn, qmn ] = vswf_coeff( obj, m, n, Qa, cosA, wList )
 %VSWF_COEFF Summary of this function goes here
 %   Detailed explanation goes here
 %   This vswf_coeff calculate Eq(50) in note.
-px=obj.incBeam.px; py=obj.incBeam.py;
-if px==2 && py==0% Radial polarized
-    
-elseif px==0 && py==2% azimuthal polarized
-    
-else
     if m~=obj.incBeam.l+1 && m~=obj.incBeam.l-1
         pmn=0.0; qmn=0.0;
     else
         import model.phy.PhysicalObject.LaserBeam.assist.gammaMN
         import model.phy.PhysicalObject.LaserBeam.assist.pi_tauMN
         
-        
+
         prefactor=0.5 * sqrt( gammaMN(m,n) );
         [pi_mn, tau_mn]=pi_tauMN(m, n, cosA);
         
@@ -26,9 +20,9 @@ else
             pmn = prefactor*(-px-1.j*py)*sum(wList.*Qa.*(pi_mn-tau_mn));
             qmn = -pmn;
         end
-        
-        
+
+
     end
-end
+
 end
 
