1913a775d4e515f94619266c97ae389f089836e6
Lei-Ming Zhou <lm.zhou2@gmail.com>
2016-07-09 16:57:42 +0800

    Azimuthal power normalization correted.
    RT.

--- +model/+phy/+PhysicalObject/+LaserBeam/+AplanaticBeam/@LinearCircularPol/calcAmpFactor.m
+++ +model/+phy/+PhysicalObject/+LaserBeam/+AplanaticBeam/@LinearCircularPol/calcAmpFactor.m
@@ -1,29 +1,35 @@
 function calcAmpFactor( obj, Ppower)
-%calcAmpFactor function get the amplitude coefficient for an incident beam 
+%calcAmpFactor function get the amplitude coefficient for an incident beam
 %with focal plane power Ppower
 %
 %   Detailed explanation goes here
-    k=obj.incBeam.k;
-    px=obj.incBeam.px; py=obj.incBeam.py;
-    Z=obj.lens.work_medium.Z;
-    n1=obj.lens.inc_medium.n;
-    n2=obj.lens.work_medium.n; 
+k=obj.incBeam.k;
+px=obj.incBeam.px; py=obj.incBeam.py;
+Z=obj.lens.work_medium.Z;
+n1=obj.lens.inc_medium.n;
+n2=obj.lens.work_medium.n;
 
-    nPiece=10;
-    [aList, wList]=obj.alpha_sampling(nPiece);
-    sinA=sin(aList); cosA=cos(aList); 
-    pp=px+1.j*py; pm=px-1.j*py;
-    p=obj.incBeam.p;l=obj.incBeam.l;
-   
-    y=sqrt(2.0)*n2*sinA/obj.f0/obj.lens.NA;
+nPiece=10;
+[aList, wList]=obj.alpha_sampling(nPiece);
+sinA=sin(aList); cosA=cos(aList);
+pp=px+1.j*py; pm=px-1.j*py;
+p=obj.incBeam.p;l=obj.incBeam.l;
+
+y=sqrt(2.0)*n2*sinA/obj.f0/obj.lens.NA;
+if (px==2 || py==2)%radially and azimuthal polarized
+    Q1a=1/Z*n1/n2*exp(-y.*y).*sinA.^3.*cosA;
+    P1=pi/k^2*y.^2./sinA.^2.*Q1a; 
+    Pfocal=wList*P1.';
+else %LG linearly and circularly polarization
     Pa=y.^abs(l) .* laguerreL(p, abs(l), y.*y);
-%     Z=377.0/n2;% The wave impedance of working medium.
+    %     Z=377.0/n2;% The wave impedance of working medium.
     Q1a=1/Z*n1/n2*Pa.^2.*exp(-y.*y).*(1+cosA).^2.*sinA;
     Q2a=1/Z*n1/n2*Pa.^2.*exp(-y.*y).*(1-cosA).^2.*sinA;
     P1=pi/4/k^2*(abs(px)^2+abs(py)^2).*Q1a;
     P2=-pi/8/k^2*(abs(pp)^2+abs(pm)^2).*Q2a;
-    Pfocal=wList*P1.'+wList*P2.'; 
-    amp=sqrt(Ppower/Pfocal)*1e6; % the 1e6 is because we use um as unit.
-    obj.AmplitudeFactor=amp;
-    obj.focBeam.setAmplitudeFactor(amp);
+    Pfocal=wList*P1.'+wList*P2.';
+end
+amp=sqrt(Ppower/Pfocal)*1e6; % the 1e6 is because we use um as unit.
+obj.AmplitudeFactor=amp;
+obj.focBeam.setAmplitudeFactor(amp);
 end
--- plotenhance/figset.m
+++ plotenhance/figset.m
@@ -10,7 +10,7 @@ set(gcf,'PaperSize',[14.4 10.5],'PaperPosition',[0.0, 0.0, 14.4, 10.5])%A6 paper
 % set(gcf,'PaperSize',[21.0,14.4],'PaperPosition',[0.0, 0.0, 21.0,14.4])%A5 paper
 
 set(gca,'Position',[0.20,0.22,0.75,0.73],'Linewidth',2) %for real number
-set(gca,'Position',[0.20,0.21,0.75,0.71])               % for y aix 10^n
+set(gca,'Position',[0.21,0.21,0.75,0.71])               % for y aix 10^n
 
 % set(gcf,'PaperSize',[24.4 10.5],'PaperPosition',[0.0, 0.0, 24.4, 10.5])
 % set(gca,'Position',[0.11,0.23,0.84,0.72],'Linewidth',2) %for real number
